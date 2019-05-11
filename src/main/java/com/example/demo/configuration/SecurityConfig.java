9d39783c49cbe5b7a98ada36206f1a5ecec8c4b4
Tobias Gottschalk <orggottschalk@web.de>
2019-05-11 11:28:43 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/user/**").hasRole("USER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/index")
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
