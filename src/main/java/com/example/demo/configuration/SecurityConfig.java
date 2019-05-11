f088059512b320b7d4b1c5e83aadd12977dc2d91
Tobias Gottschalk <orggottschalk@web.de>
2019-05-11 13:17:22 +0200

    Password encrypter hinzugefügt

--- src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.example.demo.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,14 +21,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin().loginPage("/login").defaultSuccessUrl("/index")
+                .formLogin().loginPage("/login").defaultSuccessUrl("/index.html")
                 .permitAll();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
+                //.withUser("user").password("password").roles("USER");
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER");
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
