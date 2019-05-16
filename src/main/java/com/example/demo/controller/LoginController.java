92249fba406519f18150695f0778c75600667fba
Tobias Gottschalk <orggottschalk@web.de>
2019-05-16 11:55:32 +0200

    AD Authentication funktioniert

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,19 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
     public String login(){
+
         return "login";
     }
 
     @PostMapping("login")
     public String postLogin(){
+
         return "index";
     }
 }
