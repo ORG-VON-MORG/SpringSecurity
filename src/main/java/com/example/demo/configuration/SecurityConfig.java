4c4d9d524046fd99298907fd5b9a9385137080c5
Tobias Gottschalk <orggottschalk@web.de>
2019-05-18 14:10:20 +0200

    Admin Page hinzugefügt

--- src/main/java/com/example/demo/configuration/SecurityConfig.java
+++ src/main/java/com/example/demo/configuration/SecurityConfig.java
@@ -9,147 +9,38 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  /*
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()
-                .antMatchers("/user/**").hasRole("USER")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin().loginPage("/login").defaultSuccessUrl("/index.html")
-                .permitAll();
-
-
-
-       // http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-
-
-    }
-    */
-
-    /*
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth
-                .inMemoryAuthentication()
-                //.withUser("user").password("password").roles("USER");
-                .withUser("user").password(passwordEncoder().encode("password")).roles("USER");
-    }
-
-    */
 
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-/*
-    @Bean
-    public ActiveDirectoryLdapAuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
-        ActiveDirectoryLdapAuthenticationProvider activeDirectoryLdapAuthenticationProvider = new
-                ActiveDirectoryLdapAuthenticationProvider("gps-srp.local", "ldap://192.168.178.98:389/");
-        return activeDirectoryLdapAuthenticationProvider;
-    }
 
-*/
-
-
-//-----------Von der Webseite https://stackoverflow.com/questions/42229066/spring-boot-ldap-securityhttps://stackoverflow.com/questions/42229066/spring-boot-ldap-security
-/*
-    @Override
-    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-        authManagerBuilder.authenticationProvider(activeDirectoryLdapAuthenticationProvider()).userDetailsService(userDetailsService());
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager() {
-        return new ProviderManager(Arrays.asList(activeDirectoryLdapAuthenticationProvider()));
-    }
-    @Bean
-    public AuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
-        ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider("gps-srp.local", "ldap://192.168.178.98:389/", "dc=gps-srp,dc=local");
-        provider.setSearchFilter("(&(objectClass=user)(sAMAccountName={0}))");
-        provider.setConvertSubErrorCodesToExceptions(true);
-        provider.setUseAuthenticationRequestCredentials(true);
-        return provider;
-    }
-    */
-
-//-------------------------------------------
-
-//-------------------https://www.ziaconsulting.com/developer-help/spring-security-active-directory/
-
-  /*
-@Override
-protected void configure(HttpSecurity http) throws Exception
-{
-    http
-            .authorizeRequests()
-            .anyRequest().fullyAuthenticated()
-            .and()
-            .httpBasic();
-}
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception
-    {
-        ActiveDirectoryLdapAuthenticationProvider adProvider =
-                new ActiveDirectoryLdapAuthenticationProvider("gps-srp.local","ldap://192.168.178.98:389/");
-        //adProvider.setConvertSubErrorCodesToExceptions(true);
-      //  adProvider.setUseAuthenticationRequestCredentials(true);
-
-        // set pattern if it exists
-        // The following example would authenticate a user if they were a member
-        // of the ServiceAccounts group
-        // (&(objectClass=user)(userPrincipalName={0})
-        //   (memberof=CN=ServiceAccounts,OU=alfresco,DC=mycompany,DC=com))
-
-
-        if (userDNPattern != null && userDNPattern.trim().length() > 0)
-        {
-            adProvider.setSearchFilter(userDNPattern);
-        }
-
-
-
-        //adProvider.setSearchFilter("(&(objectClass=user)(sAMAccountName={0}))");
-        auth.authenticationProvider(adProvider);
-
-        // don't erase credentials if you plan to get them later
-        // (e.g using them for another web service call)
-      //  auth.eraseCredentials(false);
-    }
-
-
-    */
-
-
-
-//----------------------------------------------
-
-
-//----------------https://medium.com/@viraj.rajaguru/how-to-use-spring-security-to-authenticate-with-microsoft-active-directory-1caff11c57f2
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                //.antMatchers("/admin/**").hasRole("ADMIN")
+                //.antMatchers("/user/**").hasRole("USER")
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .formLogin();
+    
     }
 
     @Bean
@@ -158,6 +49,10 @@ protected void configure(HttpSecurity http) throws Exception
                 ActiveDirectoryLdapAuthenticationProvider("gps-srp.local", "ldap://192.168.178.98:389/");
         return activeDirectoryLdapAuthenticationProvider;
     }
+
+
+
+
 
 }
 
--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,18 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 
+
+@Controller
 public class UserController {
 
     @GetMapping("/user")
     public String showUser(){
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         System.out.println(principal);
-        return "user/user";
+
+        return "user";
     }
 }
